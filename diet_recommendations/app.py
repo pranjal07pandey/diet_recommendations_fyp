4655b0582778486e74c05206678411bb5f617864
Pranjal Pandey <pranjalpandey92@gmail.com>
2024-05-07 17:32:55 +0100

    added script to get food images based on search term

--- diet_recommendations/app.py
+++ diet_recommendations/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from get_food_images import get_images_links
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/food-images')
+def get_food():
+    return get_images_links("virgin pina colada")
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
